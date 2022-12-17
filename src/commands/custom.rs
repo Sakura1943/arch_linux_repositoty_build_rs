4aaa86741ea99b877c5e042ef4dd9b14e485ba13
Sakura1943 <1436700265@qq.com>
2022-12-17 17:41:00 +0800

    Update src/commands/custom.rs

--- src/commands/custom.rs
+++ src/commands/custom.rs
@@ -4,7 +4,7 @@ use std::{fs::copy, path::Path};
 use subprocess::Exec;
 
 pub fn add(package_path: &Path, save_path: &str, server_name: &str) -> Result<()> {
-    if !package_path.to_string_lossy().ends_with(".pkg.tar,zst") {
+    if !package_path.to_string_lossy().ends_with(".pkg.tar.zst") {
         return Err(anyhow!(
             "Only files with the .pkg.tar.zst extension are allowed to operate."
         ));
